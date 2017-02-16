e3b81d0c54355fc56c0fc0371f13d5899ebb1768
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-02-16 16:26:33 +0200

    Maven settings

--- drumbeat_security_organization/src/main/java/fi/aalto/drumbeat/rest/RESTfulAPI.java
+++ drumbeat_security_organization/src/main/java/fi/aalto/drumbeat/rest/RESTfulAPI.java
@@ -13,6 +13,12 @@ import javax.ws.rs.core.Response;
 public class RESTfulAPI{
 	
 	@GET
+	@Path("/hello")
+    public Response hello() {
+		return Response.status(200).entity("Hello...").build();
+    }
+	
+	@GET
 	@Path("/check_user/{path}.{webID}.{timestamp}")
     public Response check_user(@PathParam("path") String path,@PathParam("webID") String webID,@PathParam("timestamp") String timestamp) {
 		return Response.status(200).entity("Just OK now").build();
