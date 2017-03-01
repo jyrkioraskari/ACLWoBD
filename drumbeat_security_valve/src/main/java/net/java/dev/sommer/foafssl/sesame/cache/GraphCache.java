a54bb0f7d7212e3f046d60a4a59348eb9b5915d4
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-03-01 17:55:43 +0200

    Debugging

--- drumbeat_security_valve/src/main/java/net/java/dev/sommer/foafssl/sesame/cache/GraphCache.java
+++ drumbeat_security_valve/src/main/java/net/java/dev/sommer/foafssl/sesame/cache/GraphCache.java
@@ -102,6 +102,7 @@ public abstract class GraphCache {
              * "file" is for debugging purposes the above could be made more
              * generic by tying it to the URLConnection lib or something.
              */
+        	log.warning("--- DRUMBEAT webid no connection explanation: "+"Cannot dereference " + scheme + " urls");
             webidClaim.fail("Cannot dereference " + scheme + " urls");
             /*
              * one could do some further logic somehow, by checking if any
@@ -116,6 +117,7 @@ public abstract class GraphCache {
             purl = webid.toURL();
             base = new URL(purl.getProtocol(), purl.getHost(), purl.getPort(), purl.getFile());
         } catch (MalformedURLException e) {
+        	log.warning("--- DRUMBEAT webid no connection explanation: "+"WebId is not one we know to dereference ");
             webidClaim.addProblem(new Error("WebId is not one we know to dereference ", e));
             /*
              * TODO: one could do other things: like look at trusted graphs...
@@ -193,6 +195,7 @@ public abstract class GraphCache {
                     foafDocInputStream = new TeeInputStream(foafDocInputStream, cacheout);
                 }
             } catch (IOException e) {
+            	log.warning("--- DRUMBEAT webid no connection explanation: "+"could not connect to resource ");
                 webidClaim.fail("could not connect to resource " + purl, e);
                 return null;
             }
@@ -203,6 +206,8 @@ public abstract class GraphCache {
             try {
                 repconn = getSailRep().getConnection();
             } catch (RepositoryException e) {
+            	log.warning("--- DRUMBEAT webid no connection explanation: "+"internal error. Could not connect to rdf repository");
+
                 webidClaim.fail("internal error. Could not connect to rdf repository", e);
             }
             try {
@@ -211,10 +216,13 @@ public abstract class GraphCache {
                 repconn.add(stream, base.toString(), rdfFormat, foafdocUri);
                 webidClaim.setGraphName(base);
                 if (stream.wasCutShort()) {
+                	log.warning("--- DRUMBEAT webid no connection explanation: "+"Input from resource was cut off at " + stream.getMax());
+
                     webidClaim.warn("Input from resource was cut off at " + stream.getMax()
                             + " Data could be missing.");
                 }
             } catch (IOException e) {
+            	log.warning("--- DRUMBEAT webid no connection explanation: "+"Could not read input from resource ");
                 webidClaim.fail("Could not read input from resource " + purl, e);
                 /*
                  * TODO: need not return null. It could try to continue and read
@@ -222,6 +230,7 @@ public abstract class GraphCache {
                  */
                 return null;
             } catch (RDFParseException e) {
+            	log.warning("--- DRUMBEAT webid no connection explanation: "+"Could not parse resource " );
                 webidClaim.fail("Could not parse resource " + purl, e);
                 /*
                  * TODO: need not return null. It could try to continue and read
@@ -229,6 +238,7 @@ public abstract class GraphCache {
                  */
                 return null;
             } catch (RepositoryException e) {
+            	log.warning("--- DRUMBEAT webid no connection explanation: "+"Internal error. Could not add information to repository");
                 webidClaim.fail("Internal error. Could not add information to repository", e);
                 return null;
             }
