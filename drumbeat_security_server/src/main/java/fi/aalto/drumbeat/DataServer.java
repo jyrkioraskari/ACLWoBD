5d10af2cad9f8bcc28b4b358ab9602c698c54176
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-03-06 13:19:02 +0200

    Test...fix

--- drumbeat_security_server/src/main/java/fi/aalto/drumbeat/DataServer.java
+++ drumbeat_security_server/src/main/java/fi/aalto/drumbeat/DataServer.java
@@ -123,7 +123,7 @@ public class DataServer {
 					}
 				}
 				
-				{
+				/*{
 					List<String> perms=x.getPermissions(r.toString()).stream().map(y->{
 						String sy=y.asResource().getURI();
 						int i=sy.lastIndexOf("/");
@@ -131,7 +131,7 @@ public class DataServer {
 						return sy;
 					}).collect(Collectors.toCollection(ArrayList::new));
 					ret.addAll(perms); //TODO test is collective
-				}
+				}*/
 			});
 
 		}
