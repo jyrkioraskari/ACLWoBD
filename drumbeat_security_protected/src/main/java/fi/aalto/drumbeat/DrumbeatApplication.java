90cb0a34d5ea9b074fb45d9c3c5d5c02e713a619
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-03-07 23:20:28 +0200

    ResourceConfig first steps

---
+++ drumbeat_security_protected/src/main/java/fi/aalto/drumbeat/DrumbeatApplication.java
@@ -0,0 +1,11 @@
+package fi.aalto.drumbeat;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class DrumbeatApplication extends ResourceConfig {
+    public DrumbeatApplication() {
+        packages("fi.aalto.drumbeat.rest");
+    }
+
+	
+}
