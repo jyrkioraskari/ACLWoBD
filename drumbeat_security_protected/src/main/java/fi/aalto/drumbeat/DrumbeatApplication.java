9b0576bb5d1e18522a7e9d4b2d4630e49e19fd72
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-03-08 00:28:22 +0200

    Test settings

--- drumbeat_security_protected/src/main/java/fi/aalto/drumbeat/DrumbeatApplication.java
+++ drumbeat_security_protected/src/main/java/fi/aalto/drumbeat/DrumbeatApplication.java
@@ -1,18 +1,12 @@
 package fi.aalto.drumbeat;
 
-import javax.ws.rs.ApplicationPath;
-
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 
 public class DrumbeatApplication extends ResourceConfig {
 	public DrumbeatApplication() {
 		packages("fi.aalto.drumbeat.rest");
-		register(JspMvcFeature.class);
-		property("jersey.config.server.mvc.templateBasePath.jsp", "/WEB-INF/jsp");
-
-		
-		
+		property("com.sun.jersey.config.property.JSPTemplatesBasePath", "/WEB-INF/jsp");
+		property("com.sun.jersey.config.property.WebPageContentRegex", "/(resources|(WEB-INF/jsp))/.*");
 	}
 
 }
