d0b69d4bd2592e93d767b7c36526a056415e52b6
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-03-10 16:08:43 +0200

    Removing deprecated method call

--- drumbeat_security_server/src/main/java/fi/aalto/drumbeat/DrumbeatApplication.java
+++ drumbeat_security_server/src/main/java/fi/aalto/drumbeat/DrumbeatApplication.java
@@ -1,6 +1,6 @@
 package fi.aalto.drumbeat;
 
-import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.MvcFeature;
 import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
@@ -8,7 +8,7 @@ import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 public class DrumbeatApplication extends ResourceConfig {
 	public DrumbeatApplication() {
 		packages("fi.aalto.drumbeat.rest");
-		register(LoggingFilter.class);
+		register(LoggingFeature.class);
         register(JspMvcFeature.class);
         property("jersey.config.server.mvc.templateBasePath", "/WEB-INF/jsp");
 
