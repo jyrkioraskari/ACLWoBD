7f67aa0527faa396c347ec5e49c9d16b10c46c8f
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-03-15 19:12:44 +0200

    Rewriting the recursive fetch

--- drumbeat_security_server/src/main/java/fi/aalto/cs/drumbeat/controllers/DrumbeatSecurityController.java
+++ drumbeat_security_server/src/main/java/fi/aalto/cs/drumbeat/controllers/DrumbeatSecurityController.java
@@ -63,7 +63,7 @@ public class DrumbeatSecurityController {
 
 	private DrumbeatSecurityController(URI uri) {
 		super();
-		rootURI =  Optional.of(uri);
+		rootURI = Optional.of(uri);
 		rdf_datastore = new RDFDataStore(rootURI.get(), "organization");
 		datamodel = rdf_datastore.getInferenceModel();
 		// rdf_datastore.readRDFData();
@@ -78,48 +78,43 @@ public class DrumbeatSecurityController {
 	}
 
 	public boolean validatePath(Resource start_node, String webid_uri, List<String> rulepath_strlist) {
-		log.info("validatePath: node "+start_node+" path: "+rulepath_strlist);
+		log.info("validatePath: node " + start_node + " path: " + rulepath_strlist);
 		DrumbeatSecurityController.getAccessList()
 				.add(new Tuple<String, Long>(
-						"validatePath: " + webid_uri + " path: " + rulepath_strlist.stream().collect(Collectors.joining("-")),
+						"validatePath: " + webid_uri + " path: "
+								+ rulepath_strlist.stream().collect(Collectors.joining("-")),
 						System.currentTimeMillis()));
 		Resource current_node = root;
 		if (start_node != null)
 			current_node = start_node;
 		ListIterator<String> iterator = rulepath_strlist.listIterator();
-		int stepper_inx=0;
-		while (iterator.hasNext()) {
-			String step = iterator.next();
-			stepper_inx++;
-			log.info("Finding property "+step+" from the inference model");
-			Property p = rdf_datastore.getInferenceModel().getProperty(step);
-			log.info("Got property "+p);
+		if (iterator.hasNext()) {
+			String first_step = iterator.next();
+			log.info("Finding property " + first_step + " from the inference model");
+			Property p = rdf_datastore.getInferenceModel().getProperty(first_step);
 			StmtIterator connected_triples = current_node.listProperties(p);
-			if(!connected_triples.hasNext())
+			if (!connected_triples.hasNext()) {
 				log.info("No triples connected to the property");
+				log.info("located here?:" + current_node);
+				log.info("Path for the rest is:" + rulepath_strlist);
+				return checkPath_HTTP(current_node.toString(), webid_uri, rulepath_strlist);
+			}
 			while (connected_triples.hasNext()) {
 				Statement triple = connected_triples.next();
 				Resource node = triple.getObject().asResource();
-				if (node != null) {
-					log.info("from local store:" + node);
-					current_node = node;
-					if (!iterator.hasNext()) {
-						if (current_node.toString().equals(webid_uri)) {
-							DrumbeatSecurityController.getAccessList().add(new Tuple<String, Long>(
-									"-->" + webid_uri + " found here", System.currentTimeMillis()));
-							return true;
-						}
-					} else {
-						List<String> new_path = rulepath_strlist.subList(stepper_inx, rulepath_strlist.size());
-						return validatePath(current_node, webid_uri, new_path);
+				log.info("from local store:" + node);
+				current_node = node;
+				if (!iterator.hasNext()) {
+					if (current_node.toString().equals(webid_uri)) {
+						DrumbeatSecurityController.getAccessList().add(
+								new Tuple<String, Long>("-->" + webid_uri + " found here", System.currentTimeMillis()));
+						return true;
 					}
 				} else {
-					log.info("located somewhere else. current node was: " + current_node);
-					
-					List<String> new_path = rulepath_strlist.subList(stepper_inx, rulepath_strlist.size());
-					log.info("Path for the rest is:" + new_path);
-					return checkPath_HTTP(current_node.toString(), webid_uri, new_path);
+					List<String> new_path = rulepath_strlist.subList(1, rulepath_strlist.size());
+					return validatePath(current_node, webid_uri, new_path);
 				}
+
 			}
 		}
 
@@ -140,12 +135,12 @@ public class DrumbeatSecurityController {
 		final OntModel query_model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
 		System.out.println("Next step URL is: " + nextStepURL);
 		try {
-			List<String> rulepath_lista=new ArrayList<>();
+			List<String> rulepath_lista = new ArrayList<>();
 			for (int i = 0; i < new_path.size(); i++) {
 				rulepath_lista.add(new_path.get(i));
 			}
-			Resource rulepath=Dumbeat_JenaLibrary.createRulePath(query_model,rulepath_lista);
-			
+			Resource rulepath = Dumbeat_JenaLibrary.createRulePath(query_model, rulepath_lista);
+
 			Resource query = query_model.createResource();
 			query.addProperty(Ontology.Authorization.hasRulePath, rulepath);
 
@@ -177,7 +172,8 @@ public class DrumbeatSecurityController {
 			@SuppressWarnings("unused")
 			RDFNode time_stamp = result.getProperty(Ontology.Message.hasTimeStamp).getObject();
 			// TODO check the time_stamp
-			return result.getProperty(Ontology.Message.hasPermissionStatus).getObject().asResource() == Ontology.Message.accepted;
+			return result.getProperty(Ontology.Message.hasPermissionStatus).getObject()
+					.asResource() == Ontology.Message.accepted;
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -189,7 +185,7 @@ public class DrumbeatSecurityController {
 		return datamodel.getResource(webid_uri.toString());
 	}
 
-	public Resource registerWebID(String webidURI, String public_key) {		
+	public Resource registerWebID(String webidURI, String public_key) {
 		Resource widr = datamodel.getResource(webidURI);
 		root.addProperty(Ontology.Contractor.trusts, widr);
 		widr.addLiteral(Ontology.property_hasPublicKey, public_key);
@@ -197,7 +193,6 @@ public class DrumbeatSecurityController {
 		return widr;
 	}
 
-	
 	/*
 	public Model getWebID(String webid) {
 		//http://stackoverflow.com/questions/1820908/how-to-turn-off-the-eclipse-code-formatter-for-certain-sections-of-java-code
